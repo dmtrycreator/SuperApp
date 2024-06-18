5135a0c5cbf3572f179e8bc64696ae2d84611a6f
Дмитрий <167537455+dmtrycreator@users.noreply.github.com>
2024-06-18 17:51:50 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Установка необходимых пакетов
 sudo apt update
 sudo apt install -y openjdk-21-jdk git maven unzip
@@ -32,7 +33,7 @@ sudo mkdir -p /opt/javafx
 cd /opt/javafx || exit
 sudo curl -L -o openjfx.zip https://download2.gluonhq.com/openjfx/22.0.1/openjfx-22.0.1_linux-x64_bin-sdk.zip
 sudo unzip -o openjfx.zip
-sudo mv javafx-sdk-22.0.1/lib ./lib
+sudo cp -r javafx-sdk-22.0.1/lib/* ./lib
 sudo rm -rf javafx-sdk-22.0.1 openjfx.zip
 
 # Копирование исполняемых файлов
@@ -53,26 +54,5 @@ Categories=Utility;"
 
 echo "$DESKTOP_ENTRY" > ~/.local/share/applications/SuperApp.desktop
 
-# Установочные директории
-INSTALL_DIR="$HOME/.superapp"
-BASHRC="$HOME/.bashrc"
-
-# Создаем директории, если их нет
-mkdir -p "$INSTALL_DIR/scripts"
-
-# Копируем файлы в установочную директорию
-if [ -d "$HOME/SuperApp/scripts" ]; then
-    cp -r "$HOME/SuperApp/scripts/*" "$INSTALL_DIR/scripts/"
-fi
-
-# Добавляем команды в .bashrc, если они еще не добавлены
-if ! grep -q "source $INSTALL_DIR/scripts/custom_bashrc" "$BASHRC"; then
-    echo "source $INSTALL_DIR/scripts/custom_bashrc" >> "$BASHRC"
-    echo "Пользовательские команды SuperApp добавлены в $BASHRC"
-fi
-
-# Применяем изменения
-source "$BASHRC"
-
 # Уведомление об успешной установке
-echo "Вы можете запустить приложение из меню или командой: java --module-path /opt/javafx/lib --add-modules javafx.controls,javafx.fxml -jar $HOME/SuperApp/target/KR_SuperApp-1.0-SNAPSHOT.jar"
+echo "Вы можете запустить приложение из меню или командой: java --module-path /opt/javafx/lib --add-modules javafx.controls,javafx.fxml -jar $HOME/SuperApp/target/KR_SuperApp-1.0-SNAPSHOT-shaded.jar"
