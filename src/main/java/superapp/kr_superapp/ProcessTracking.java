36be838bff98f0d0d46c56128a5c1bc6140751fe
Дмитрий <167537455+dmtrycreator@users.noreply.github.com>
2024-06-22 21:37:30 +0400

    Update ProcessTracking.java

--- src/main/java/superapp/kr_superapp/ProcessTracking.java
+++ src/main/java/superapp/kr_superapp/ProcessTracking.java
@@ -14,7 +14,6 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
