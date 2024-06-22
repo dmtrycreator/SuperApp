e11794872407b255e2181c71f210ed695002a72b
Дмитрий <167537455+dmtrycreator@users.noreply.github.com>
2024-06-22 21:38:43 +0400

    Update SystemInfoApp.java

--- src/main/java/superapp/kr_superapp/SystemInfoApp.java
+++ src/main/java/superapp/kr_superapp/SystemInfoApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class SystemInfoApp extends Application {
@@ -15,6 +16,7 @@ public class SystemInfoApp extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         primaryStage.setTitle("System Info");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/superapp/kr_superapp/icons/System.png")));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
