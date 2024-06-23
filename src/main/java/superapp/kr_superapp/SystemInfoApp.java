05ec29cca74f5ae3aabc7f6edae03663e9670431
Дмитрий <167537455+dmtrycreator@users.noreply.github.com>
2024-06-23 13:59:30 +0400

    Update SystemInfoApp.java

--- src/main/java/superapp/kr_superapp/SystemInfoApp.java
+++ src/main/java/superapp/kr_superapp/SystemInfoApp.java
@@ -11,20 +11,21 @@ public class SystemInfoApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        System.out.println("SystemInfoApp: start() method called");
+        String processName = getParameters().getUnnamed().size() > 1 ? getParameters().getUnnamed().get(1) : "System Info Process";
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("SystemInfo.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-        primaryStage.setTitle("System Info");
+        primaryStage.setTitle("Информация о системе");
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/superapp/kr_superapp/icons/System.png")));
         primaryStage.setScene(scene);
         primaryStage.show();
-        System.out.println("SystemInfoApp: Stage shown");
     }
 
     public static void main(String[] args) {
-        System.out.println("SystemInfoApp: main() method called with args: " + String.join(", ", args));
         launch(args);
     }
 }
+
+// Similarly update ProcessTrackingApp and TerminalApp
