d6358b09a60f9924d478c12ddef7c7f3836716d1
Дмитрий <167537455+dmtrycreator@users.noreply.github.com>
2024-06-23 15:46:41 +0400

    Update ProcessTrackingApp.java

--- src/main/java/superapp/kr_superapp/ProcessTrackingApp.java
+++ src/main/java/superapp/kr_superapp/ProcessTrackingApp.java
@@ -11,17 +11,16 @@ public class ProcessTrackingApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        String processName = getParameters().getUnnamed().size() > 1 ? getParameters().getUnnamed().get(1) : "Process Tracking Process";
+        
         FXMLLoader loader = new FXMLLoader(getClass().getResource("ProcessTracking.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-        primaryStage.setTitle("Process Tracking");
+        primaryStage.setTitle("Отслеживание процессов");
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/superapp/kr_superapp/icons/Process.png")));
         primaryStage.setScene(scene);
-
-        // Logging for debugging
-        System.out.println("FXML file loaded successfully.");
-
+        
         FileMappingHandler fileMappingHandler = new FileMappingHandler();
         String sharedData = new String(fileMappingHandler.readData()).trim();
         System.out.println("Shared data: " + sharedData);
